0a5e6f61fa648d93419a9fd65be763b07b5feb06
Albatraoz12 <dimosthenis.emmanouil@chasacademy.se>
2024-07-04 20:26:21 +0200

    fetch data and display it to homepage without styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,44 @@
 import supabase from '../config/supabaseClient';
+import { useEffect, useState } from 'react';
 
 const Home = () => {
+  const [fetchError, setFetchError] = useState(null);
+  const [smoothies, setSmoothies] = useState(null);
+
+  useEffect(() => {
+    const fetchSmoothies = async () => {
+      const { data, error } = await supabase.from('smoothies').select();
+
+      if (error) {
+        setFetchError('Could not fetch smoothies');
+        setSmoothies(null);
+        console.log(error);
+      }
+
+      if (data) {
+        setSmoothies(data);
+        setFetchError(null);
+        console.log(data);
+      }
+    };
+
+    fetchSmoothies();
+  }, []);
+
   console.log(supabase);
 
-  return <div>Home</div>;
+  return (
+    <div className='home-page'>
+      {fetchError && <p>{fetchError}</p>}
+      {smoothies && (
+        <div className='smoothies'>
+          {smoothies.map((smoothie, index) => (
+            <p key={index}>{smoothie.title}</p>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Home;
