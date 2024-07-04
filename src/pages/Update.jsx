22afad33b59ecae417e6503e6a227ba41d8caef1
Albatraoz12 <dimosthenis.emmanouil@chasacademy.se>
2024-07-04 21:50:31 +0200

    fetch a smoothie by id and log it into console

--- src/pages/Update.jsx
+++ src/pages/Update.jsx
@@ -1,7 +1,38 @@
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import supabase from '../config/supabaseClient';
 
 const Update = () => {
   const { id } = useParams();
+  const navigate = useNavigate();
+
+  const [title, setTitle] = useState('');
+  const [method, setMethod] = useState('');
+  const [rating, setRating] = useState('');
+
+  useEffect(() => {
+    const fetchSmoothie = async () => {
+      const { data, error } = await supabase
+        .from('smoothies')
+        .select()
+        .eq('id', id)
+        .single();
+
+      if (error) {
+        navigate('/', { replace: true });
+        console.log(error);
+      }
+
+      if (data) {
+        setTitle(data.title);
+        setMethod(data.method);
+        setRating(data.rating);
+        console.log(data);
+      }
+    };
+
+    fetchSmoothie();
+  }, [id, navigate]);
 
   return (
     <div>
