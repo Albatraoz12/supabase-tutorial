6ed9493dbef720236bf0aae79a9c09222d31c4b3
Albatraoz12 <dimosthenis.emmanouil@chasacademy.se>
2024-07-04 21:11:49 +0200

    add link to update page with smoothie id

--- src/components/SmoothieCard.jsx
+++ src/components/SmoothieCard.jsx
@@ -1,4 +1,6 @@
 /* eslint-disable react/prop-types */
+import { Link } from 'react-router-dom';
+
 /* eslint-disable no-unused-vars */
 const SmoothieCard = ({ smoothie }) => {
   return (
@@ -6,6 +8,11 @@ const SmoothieCard = ({ smoothie }) => {
       <h3>{smoothie.title}</h3>
       <p>{smoothie.method}</p>
       <div className='rating'>{smoothie.rating}</div>
+      <div className='buttons'>
+        <Link to={'/' + smoothie.id}>
+          <i className='material-icons'>edit</i>
+        </Link>
+      </div>
     </div>
   );
 };
