f362c1970d97a0336cee2e4448c0ea05f4848edb
Albatraoz12 <dimosthenis.emmanouil@chasacademy.se>
2024-07-04 22:00:27 +0200

    add functionallity to update a smoothie

--- src/pages/Update.jsx
+++ src/pages/Update.jsx
@@ -9,6 +9,32 @@ const Update = () => {
   const [title, setTitle] = useState('');
   const [method, setMethod] = useState('');
   const [rating, setRating] = useState('');
+  const [formError, setFormError] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!title || !method || !rating) {
+      setFormError('Please fill in all feilds correctly');
+      return;
+    }
+
+    const { data, error } = await supabase
+      .from('smoothies')
+      .update({ title, method, rating })
+      .eq('id', id)
+      .select();
+
+    if (error) {
+      setFormError('Please fill in all feilds correctly');
+      console.log(error);
+    }
+
+    if (data) {
+      setFormError(null);
+      navigate('/');
+    }
+  };
 
   useEffect(() => {
     const fetchSmoothie = async () => {
@@ -36,7 +62,7 @@ const Update = () => {
 
   return (
     <div className='page create'>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor='title'>Title:</label>
         <input
           type='text'
@@ -61,6 +87,7 @@ const Update = () => {
         />
 
         <button>Update Smoothie Recipe</button>
+        {formError && <p className='error'>{formError}</p>}
       </form>
     </div>
   );
