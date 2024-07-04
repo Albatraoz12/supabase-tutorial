8797a06ba6c6ae6c6ca3d3b7a2a1a1f3a95ade46
Albatraoz12 <dimosthenis.emmanouil@chasacademy.se>
2024-07-04 20:38:31 +0200

    style card component and general

--- src/components/SmoothieCard.jsx
+++ src/components/SmoothieCard.jsx
@@ -2,7 +2,7 @@
 /* eslint-disable no-unused-vars */
 const SmoothieCard = ({ smoothie }) => {
   return (
-    <div>
+    <div className='smoothie-card'>
       <h3>{smoothie.title}</h3>
       <p>{smoothie.method}</p>
       <div className='rating'>{smoothie.rating}</div>
