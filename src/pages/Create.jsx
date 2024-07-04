6255062c487327a38cdf7f8d31806c95b153ef2b
Albatraoz12 <dimosthenis.emmanouil@chasacademy.se>
2024-07-04 20:59:02 +0200

    let users create a smoothie with functionallity and no styling

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,5 +1,70 @@
+import { useState } from 'react';
+import supabase from '../config/supabaseClient';
+
 const Create = () => {
-  return <div>Create</div>;
+  const [title, setTitle] = useState('');
+  const [method, setMethod] = useState('');
+  const [rating, setRating] = useState('');
+  const [formError, setFormError] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!title || !method || !rating) {
+      setFormError('Please fill in all feilds correctly');
+      return;
+    }
+
+    const { data, error } = await supabase
+      .from('smoothies')
+      .insert([{ title, method, rating }])
+      .select();
+
+    if (error) {
+      setFormError('Please fill in all feilds correctly');
+      console.log(error);
+    }
+    if (data) {
+      console.log(data);
+      setFormError(null);
+      setTitle('');
+      setMethod('');
+      setRating('');
+    }
+  };
+
+  return (
+    <div className='page create'>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='title'>Title:</label>
+        <input
+          type='text'
+          id='title'
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <label htmlFor='method'>Method:</label>
+        <textarea
+          id='method'
+          value={method}
+          onChange={(e) => setMethod(e.target.value)}
+        />
+
+        <label htmlFor='rating'>Rating:</label>
+        <input
+          type='number'
+          id='rating'
+          value={rating}
+          onChange={(e) => setRating(e.target.value)}
+        />
+
+        <button>Create Smoothie Recipe</button>
+
+        {formError && <p className='error'>{formError}</p>}
+      </form>
+    </div>
+  );
 };
 
 export default Create;
